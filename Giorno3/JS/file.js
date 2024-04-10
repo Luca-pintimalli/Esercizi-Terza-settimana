1994bf00320d7f25ccaa7ed6d2f6cdb2ce0f1b41
Luca-pintimalli <163635386+Luca-pintimalli@users.noreply.github.com>
2024-04-10 17:02:49 +0200

    Add files via upload

---
+++ Giorno3/JS/file.js
@@ -0,0 +1,17 @@
+const groupFunction = function (e) {
+    // invoca la funzione che genera la lista con il nuovo
+    // invoca la funzione che cambia la classe (aggiunge sbarrato)
+    // invoca la funzione che elimina il task
+}
+
+let textForm = document.getElementById("form");
+let text = document.getElementById("text");
+let btn = document.getElementById("btn");
+
+
+let addNewTask = function () {
+    let taskDaCompletare = document.getElementById("taskDaCompletare");
+
+
+
+}
